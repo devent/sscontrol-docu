800000e68288825147d58509fd862a607e5e6491
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-14 18:13:41 +0100

    Initial commit

---
+++ database_service_script.mm
@@ -0,0 +1,159 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1329133574707" ID="ID_1726505626" MODIFIED="1360610432400" TEXT="Database Script">
+<edge STYLE="bezier"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+<node CREATED="1329133589940" ID="ID_1213245526" MODIFIED="1360610432400" POSITION="right" TEXT="database">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1334720731402" ID="ID_687016888" MODIFIED="1360610432400" TEXT="debugging">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1334720739893" ID="ID_989551248" MODIFIED="1360610432400" TEXT="Default false">
+<edge STYLE="bezier"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+</node>
+<node CREATED="1334720744529" ID="ID_503653180" MODIFIED="1360610432400" TEXT="boolean">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1334720759487" ID="ID_1038340681" MODIFIED="1360610432400" TEXT="true">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1334720761263" ID="ID_947098523" MODIFIED="1360610432400" TEXT="false">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+</node>
+</node>
+<node CREATED="1329133630153" ID="ID_116194451" MODIFIED="1360610432400" TEXT="bind_address">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1329134880244" ID="ID_829165728" MODIFIED="1360610432400" TEXT="Default &quot;127.0.0.1&quot;">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+</node>
+<node CREATED="1329133697721" ID="ID_734704812" MODIFIED="1360610432400" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1329133730009" ID="ID_1302914741" MODIFIED="1360610432400" TEXT="admin_password">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329133744729" ID="ID_1487425824" MODIFIED="1360610432400" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1329133996242" ID="ID_1223173541" MODIFIED="1360610432400" TEXT="database">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329134008337" ID="ID_860498321" MODIFIED="1360610432400" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1340184952377" ID="ID_1535476911" MODIFIED="1360610432400" TEXT="character_set">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1340184995870" ID="ID_282232569" MODIFIED="1360610432399" TEXT="Default &quot;utf8&quot;">
+<edge STYLE="bezier"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+</node>
+<node CREATED="1329134008337" ID="ID_1005223911" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1340184952377" ID="ID_735041621" MODIFIED="1360610432399" TEXT="collate">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1340185004193" ID="ID_742548345" MODIFIED="1360611783712" TEXT="Default &quot;utf8_general_ci&quot;">
+<edge STYLE="bezier"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+</node>
+<node CREATED="1329134008337" ID="ID_956945220" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1360609885410" ID="ID_1263414272" MODIFIED="1360610432399" TEXT="import_sql">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1360609892907" ID="ID_428620690" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1360610000770" ID="ID_1106454522" MODIFIED="1360610432399" TEXT="File">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1360609915642" ID="ID_371007414" MODIFIED="1360610432399" TEXT="URL">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1360609923981" ID="ID_203356434" MODIFIED="1360610432399" TEXT="URI">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1329134056601" ID="ID_434084560" MODIFIED="1360610432399" TEXT="user">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329134072337" ID="ID_1758019529" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1329134087585" ID="ID_1578167934" MODIFIED="1360610432399" TEXT="password">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329134089834" ID="ID_1851480388" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1335086754996" ID="ID_1297336247" MODIFIED="1360610432399" TEXT="server">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1335086757169" ID="ID_1220079399" MODIFIED="1360610432399" TEXT="Default &quot;localhost&quot;">
+<edge STYLE="bezier"/>
+<font NAME="DejaVu Sans" SIZE="12"/>
+</node>
+<node CREATED="1335086911162" ID="ID_1634625076" MODIFIED="1360610432399" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1335086822805" ID="ID_503522635" MODIFIED="1360610432399" TEXT="use_database">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1335086835717" ID="ID_194096982" MODIFIED="1360610432398" TEXT="string">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
