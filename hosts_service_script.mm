d5feb37d67ff300e702651073d6b8993ede68a4a
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-17 14:12:11 +0100

    Document hosts

---
+++ hosts_service_script.mm
@@ -0,0 +1,33 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1329133574707" ID="ID_1726505626" MODIFIED="1335405216762" TEXT="Hosts Script">
+<font NAME="DejaVu Sans" SIZE="12"/>
+<node CREATED="1329133589940" ID="ID_1213245526" MODIFIED="1335405227211" POSITION="right" TEXT="hosts">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329133730009" ID="ID_1302914741" MODIFIED="1335405232241" TEXT="ip">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329133744729" ID="ID_1487425824" MODIFIED="1335405234149" TEXT="string">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1335405250821" ID="ID_434764477" MODIFIED="1335405413777" TEXT="host">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1335405276462" ID="ID_1842079474" MODIFIED="1335405420350" TEXT="string">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1335405339912" ID="ID_318337161" MODIFIED="1335405416257" TEXT="alias">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node CREATED="1335405357232" ID="ID_1276975876" MODIFIED="1361098314727" TEXT="list">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
