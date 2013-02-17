42a1fb3c161efe2db0be2728dbc967b3f2d887fa
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-17 11:45:24 +0100

    Document hostname

---
+++ hostname_service_script.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1329133574707" ID="ID_1726505626" MODIFIED="1335343700672" TEXT="Hostname Script">
+<font NAME="DejaVu Sans" SIZE="12"/>
+<node CREATED="1329133589940" ID="ID_1213245526" MODIFIED="1335338869708" POSITION="right" TEXT="hostname">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329133730009" ID="ID_1302914741" MODIFIED="1335338882908" TEXT="set">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1329133744729" ID="ID_1487425824" MODIFIED="1361096175199" TEXT="string">
+<font BOLD="true" ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
