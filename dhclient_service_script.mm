186d7163c340134b0bef569b6411ae0247f1850f
Erwin Mueller <erwin.mueller@deventm.org>
2013-07-11 09:37:18 +0200

    Document dhclient service

---
+++ dhclient_service_script.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#2121d3" CREATED="1329133574707" ID="ID_1726505626" MODIFIED="1373486086455" TEXT="dhclient">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node COLOR="#2121d3" CREATED="1329133730009" ID="ID_1302914741" MODIFIED="1373486109074" POSITION="right" TEXT="requests">
+<edge STYLE="bezier"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1373394399256" ID="ID_1862109336" MODIFIED="1373486120818" TEXT="requests">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1329134056601" ID="ID_434084560" MODIFIED="1373486110150" POSITION="right" TEXT="prepend">
+<edge STYLE="bezier" WIDTH="2"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1329134072337" ID="ID_1758019529" MODIFIED="1373486128490" TEXT="option">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<node COLOR="#43a14b" CREATED="1329134089834" ID="ID_1851480388" MODIFIED="1373486141116" TEXT="desclaration">
+<edge STYLE="bezier"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
