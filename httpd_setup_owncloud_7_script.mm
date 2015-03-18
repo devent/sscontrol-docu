4d76b65d925b24386ad875d4f00325ee9bb4b0d5
Erwin Mueller <erwin.mueller@deventm.org>
2015-03-18 14:09:35 +0100

    Document ownCloud

---
+++ httpd_setup_owncloud_7_script.mm
@@ -0,0 +1,182 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#2121d3" CREATED="1329133574707" ID="ID_1726505626" MODIFIED="1385899732708" TEXT="setup">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1385898886725" ID="ID_1640803100" MODIFIED="1426195967389" POSITION="right" STYLE="fork" TEXT="&quot;owncloud_7&quot;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<node COLOR="#2121d3" CREATED="1391089700174" ID="ID_248319937" MODIFIED="1401575291950" TEXT="id">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1391089701195" ID="ID_1988488029" MODIFIED="1401575291950" TEXT="id">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1385899809592" ID="ID_514650890" MODIFIED="1401575291950" TEXT="alias">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1385899810746" ID="ID_1014667373" MODIFIED="1401575291950" TEXT="alias">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1401575538220" ID="ID_1593150638" MODIFIED="1401575562204" TEXT="ref">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1401575541987" ID="ID_1819213400" MODIFIED="1401575565384" TEXT="reference">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1401575549974" ID="ID_1978377253" MODIFIED="1401575562591" TEXT="refdomain">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1401575557592" ID="ID_838153297" MODIFIED="1401575565948" TEXT="reference">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1401574794646" ID="ID_1728348559" MODIFIED="1406736259285" TEXT="prefix">
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1401574796132" ID="ID_1565359839" MODIFIED="1406736262761" TEXT="prefix">
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425390839059" ID="ID_1218691066" MODIFIED="1425391163385" POSITION="right" STYLE="fork" TEXT="override">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#2121d3" CREATED="1425390843672" ID="ID_1569501632" MODIFIED="1425391157183" STYLE="fork" TEXT="mode:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node CREATED="1425390866755" ID="ID_1867356470" MODIFIED="1425391151794" STYLE="fork" TEXT="yes">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1425390868257" ID="ID_1451396346" MODIFIED="1425391151797" STYLE="fork" TEXT="no">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1425391133400" ID="ID_743250581" MODIFIED="1425391151796" STYLE="fork" TEXT="override">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1425391136489" ID="ID_340281371" MODIFIED="1425391151795" STYLE="fork" TEXT="update">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+<node CREATED="1426193197428" ID="ID_55763258" MODIFIED="1426193222303" STYLE="fork" TEXT="upgrade">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="down"/>
+</node>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425391177027" ID="ID_651951394" MODIFIED="1425391252897" POSITION="right" STYLE="fork" TEXT="backup">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#2121d3" CREATED="1425391181011" ID="ID_142583557" MODIFIED="1425391240488" STYLE="fork" TEXT="target:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node COLOR="#43a14b" CREATED="1425391182566" ID="ID_1802236313" MODIFIED="1425391266020" STYLE="fork" TEXT="target">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425391193101" ID="ID_1333323199" MODIFIED="1425391252896" POSITION="right" STYLE="fork" TEXT="debug">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1425391199005" ID="ID_587479334" MODIFIED="1425391266025" STYLE="fork" TEXT="module">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<node COLOR="#2121d3" CREATED="1425391208588" ID="ID_231074584" MODIFIED="1425391277303" STYLE="fork" TEXT="level:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="down"/>
+<node COLOR="#43a14b" CREATED="1425391211087" ID="ID_1223309189" MODIFIED="1425391266014" STYLE="fork" TEXT="level">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1425391218687" ID="ID_504428906" MODIFIED="1425391266021" TEXT="..."/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425227181026" ID="ID_1101802223" MODIFIED="1425227245354" POSITION="right" STYLE="fork" TEXT="database">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<node COLOR="#43a14b" CREATED="1425227183866" ID="ID_1716934603" MODIFIED="1425227303295" STYLE="fork" TEXT="database">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+<node COLOR="#2121d3" CREATED="1425227206556" ID="ID_713660966" MODIFIED="1426196028125" STYLE="fork" TEXT="adapter:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1425227207452" ID="ID_54353231" MODIFIED="1426196031174" STYLE="fork" TEXT="adapter">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425227189001" ID="ID_1810065054" MODIFIED="1425331357492" STYLE="fork" TEXT="user:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1425227189900" ID="ID_244238698" MODIFIED="1425227303288" STYLE="fork" TEXT="name">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425227194684" ID="ID_832684252" MODIFIED="1425300975800" STYLE="fork" TEXT="password:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1425227195594" ID="ID_955228929" MODIFIED="1425227303276" STYLE="fork" TEXT="password">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1425227200939" ID="ID_612605529" MODIFIED="1425227291450" STYLE="fork" TEXT="host:">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1425227202093" ID="ID_391254498" MODIFIED="1425227303292" STYLE="fork" TEXT="host">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#2121d3" CREATED="1426196036593" ID="ID_1420088740" MODIFIED="1426196049544" STYLE="fork" TEXT="prefix">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+<icon BUILTIN="forward"/>
+<icon BUILTIN="help"/>
+<node COLOR="#43a14b" CREATED="1426196038389" ID="ID_1538900389" MODIFIED="1426196053275" STYLE="fork" TEXT="prefix">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" ITALIC="true" NAME="DejaVu Sans Mono" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
